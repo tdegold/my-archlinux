3042e66d49758ea26d91a9a9a15f8750645354bd
tdegold <timdegold@gmx.net>
2021-10-15 16:34:48 +0200

    added install

--- 0-base.sh
+++ 0-base.sh
@@ -5,23 +5,23 @@
 # GitHub: https://github.com/tdegold/my-archlinux/
 # Contributors: Tim Degold
 
-# Env. variables #
-DISK='/dev/sda'
-DISK_SUFIX=''
+# variables #
+disk='/dev/sda'
+disk_sufix=''
 
 timedatectl set-ntp true
 
-echo "label: gpt
+echo 'label: gpt
       size=500MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name="efi"
       type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="arch_root"
-      " | sfdisk --force $DISK
+      ' | sfdisk --force $disk
 
-mkfs.fat -F32 ${DISK}${DISK_SUFIX}1
-mkfs.ext4 ${DISK}${DISK_SUFIX}2
+mkfs.fat -F32 ${disk}${disk_sufix}1
+mkfs.ext4 ${disk}${disk_sufix}2
 
-mount ${DISK}${DISK_SUFIX}2 /mnt
+mount ${disk}${disk_sufix}2 /mnt
 mkdir -p /mnt/boot/efi
-mount ${DISK}${DISK_SUFIX}1 /mnt/boot/efi
+mount ${disk}${disk_sufix}1 /mnt/boot/efi
 
 pacstrap /mnt base base-devel linux linux-firmware vim git
 
---
+++ 1-install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# Script name: 1-install.sh
+# Description: Second part of my Arch Linux installation script
+# GitHub: https://github.com/tdegold/my-archlinux/
+# Contributors: Tim Degold
+
+# variables #
+timezone=/Europe/Vienna
+lang=en_US.UTF-8
+locale=${lang}UTF-8
+keymap=de-latin1
+hostname=arch
+root_password=password
+
+ln -sf /usr/share/zoneinfo/${timezone} /etc/localtime
+
+hwclock --systohc
+
+echo ${locale} >> /etc/locale.gen
+locale-gen
+
+echo LANG=${lang} >> /etc/locale.conf
+
+echo KEYMAP=${keymap} >> /etc/vconsole.conf
+
+echo ${hostname} >> /etc/hostname
+
+echo 127.0.0.1  localhost >> /etc/hosts
+echo ::1		localhost >> /etc/hosts
+echo 127.0.1.1	${hostname} >> /etc/hosts
+
+echo root:${root_password} | chpasswd
+
+useradd -m ${username}
+echo ${username}:${user_password} | chpasswd
+usermod -aG wheel ${username}
+echo %wheel ALL=(ALL) ALL >> /etc/sudoers.tmp
