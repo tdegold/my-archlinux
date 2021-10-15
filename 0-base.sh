a34c18af14e767f8b785b316789c6393aac85dca
tdegold <timdegold@gmx.net>
2021-10-15 16:06:45 +0200

    added sfdisk command

---
+++ 0-base.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Script name: 0-base.sh
+# Description: First part of my Arch Linux installation script
+# GitHub: https://github.com/tdegold/my-archlinux/
+# Contributors: Tim Degold
+
+# Env. variables #
+DISK=/dev/nvme1
+
+timedatectl set-ntp true
+
+echo "label: gpt
+      size=500MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name="efi"
+      type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="arch_root"
+      " | sfdisk --force ${DISK} -
+
