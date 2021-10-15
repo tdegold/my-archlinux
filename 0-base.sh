fe2310fd8bf2a77cf3ece942765e9c2511f9cc96
tdegold <timdegold@gmx.net>
2021-10-15 16:11:55 +0200

    added formating for boot partition

--- 0-base.sh
+++ 0-base.sh
@@ -6,12 +6,14 @@
 # Contributors: Tim Degold
 
 # Env. variables #
-DISK=/dev/nvme1
+DISK='/dev/sda'
+DISK_SUFIX=''
 
 timedatectl set-ntp true
 
 echo "label: gpt
       size=500MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name="efi"
       type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="arch_root"
-      " | sfdisk --force ${DISK} -
+      " | sfdisk --force $DISK
 
+mkfs.fat -F32 ${DISK}${DISK_SUFIX}1
